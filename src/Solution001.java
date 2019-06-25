0badb588f02e70d54e2e486445f6076805e24c1e
nuochengtian <u6888414@anu.edu.au>
2019-06-25 16:54:38 +0800

    002

---
+++ src/Solution001.java
@@ -0,0 +1,24 @@
+class Solution001 {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        for (int i = 0;i<nums.length-1;i++){
+            for (int j = i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    solution[0] = i;
+                    solution[1] = j;
+                    return solution;
+                }
+            }
+        }
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,6,7,8,9};
+        int target = 16;
+        int[] result = twoSum(nums,target);
+        for (int i:result){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Solution002.java
@@ -0,0 +1,16 @@
+
+ class Solution002 {
+     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return add(l1,l2,0);
+     }
+
+     public ListNode add(ListNode l1,ListNode l2,int a){
+         if (l1==null&&l2==null&&a==0) return null;
+         int x = l1==null ? 0 : l1.val;
+         int y = l2==null ? 0 : l2.val;
+         int sum = x+y+a;
+         ListNode result = new ListNode(sum%10);
+         result.next = add(l1==null?null:l1.next,l2==null?null:l2.next,sum/10);
+         return result;
+     }
+ }
